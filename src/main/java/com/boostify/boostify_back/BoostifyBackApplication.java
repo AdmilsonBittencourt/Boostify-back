22fe632a5a623773d69a22b869e54ef0f8609d21
admilson <bittencourta942@gmail.com>
2024-10-20 16:10:32 -0300

    first commit

---
+++ src/main/java/com/boostify/boostify_back/BoostifyBackApplication.java
@@ -0,0 +1,13 @@
+package com.boostify.boostify_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostifyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostifyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boostify/boostify_back/BoostifyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boostify.boostify_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostifyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
