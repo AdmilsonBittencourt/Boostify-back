2a1111273b6aef602d4344c221bbc6ba92ce81c0
admilson <bittencourta942@gmail.com>
2024-12-01 14:46:18 -0300

    alterando as permições

--- src/main/java/com/boostify/boostify_back/infra/security/SecurityConfig.java
+++ src/main/java/com/boostify/boostify_back/infra/security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/tasks/**").permitAll()
+                        // .requestMatchers(HttpMethod.PUT, "/tasks/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
